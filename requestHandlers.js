b59f2a15fe6962ecad7cf881195e1a8340cb3d68
David Eliason <davethemaker@gamil.com>
2017-06-02 16:48:06 -0700

    successfully refactored formidable using doc example to upload a different file and display utility info, such as path of file

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var querystring = require('querystring');
+var formidable = require('formidable');
+var util = require('util');
 
 function start(response) {
     console.log("request handler for 'start' has been called");
@@ -12,16 +14,20 @@ function start(response) {
 function upload(response,request){
     console.log("request handler for 'upload' has been called");
 
-    // var form = new formidable.IncomingForm();
+    var form = new formidable.IncomingForm(); // from documentation
+
     console.log("about to parse");
-    // form.parse(request, function(err,fields,files){
-    //     console.log("parsing done");
-    // });
-    response.writeHead(200,{'Content-Type':'text/html'});
-    // response.write("you sent: " + querystring.parse(postData).text);
-    // response.write("<img src='/show' />");
-    response.write("<h3>show image here</h3>")
-    response.end();
+
+    form.parse(request, function(err,fields,files){ // from documentation
+        console.log("parsing done");
+        response.writeHead(200,{'Content-Type':'text/plain'});
+        // response.write("you sent: " + querystring.parse(postData).text);
+        response.write("uploaad received");
+        // response.write("<h3>show image here</h3>")
+        response.end(util.inspect({fields:fields, files:files}));
+        response.end();
+    });
+   return;
 }
 
 function show(response){
