c3c4c47acf8d4fa9cd22129ce88e38f1ccd3ab12
David Eliason <davethemaker@gamil.com>
2017-06-02 08:53:33 -0700

    grab url pathname data from recuest

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var http = require('http');
 var fs   = require('fs');
+var url  = require('url');
 
 function start() {
-    function requesListener(request,response) {
+    function onRequest(request,response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("rec'd: " + pathname);
         response.writeHead(200,{'Content-Type':'text/plain'});
         response.write("hello world");
         response.end();
     }
 
-    http.createServer(requesListener).listen(8080);
+    http.createServer(onRequest).listen(8080);
     console.log("server up at 8080");
 }
 
