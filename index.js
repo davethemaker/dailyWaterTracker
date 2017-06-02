0e09d44e37083d310b360ef67fd5890f9fc75690
David Eliason <davethemaker@gamil.com>
2017-06-02 08:42:03 -0700

    export server module and successfully invoke its importation in main file (index.js)

---
+++ index.js
@@ -0,0 +1,3 @@
+var server = require('./server.js').serverWrapper; // function in exported module
+
+server(); // instantiate server
---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs   = require('fs');
+
+function serverWrapper() {
+    function requesListener(request,response) {
+        response.writeHead(200,{'Content-Type':'text/plain'});
+        response.write("hello world");
+        response.end();
+    }
+
+    http.createServer(requesListener).listen(8080);
+    console.log("server up at 8080");
+}
+
+exports.serverWrapper = serverWrapper;
