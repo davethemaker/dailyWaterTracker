631b13685be9b50a46f2599d109b3108b37b49ac
David Eliason <davethemaker@gamil.com>
2017-06-02 09:32:02 -0700

    Inject server response object into the router, so that if there is  routing match to the requested pathname then the response object will be available to that function call so that the response can be invoked immediately

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 
-function route(handle, pathname){
+function route(handle, pathname,response){
     console.log("here is the pathname, going to route to it: " + pathname);
     if(typeof handle[pathname] === 'function'){
-        handle[pathname](); // invoke the function
+        handle[pathname](response); // invoke the function
     }
     else{
         console.log("no request handler found for " + pathname);
+        response.writeHead(404,{'Content-Type':'text/plain'});
+        response.write("404 not found");
+        response.end();
     }
 }
 
